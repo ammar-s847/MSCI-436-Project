2a04ee56978d2679963f5709dc8e2f18ba54011b
ammar siddiqui <ammar.siddiqui847@gmail.com>
2024-07-10 19:53:21 -0400

    updates

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ from flask_socketio import SocketIO
 
 from time_series.garch import (
     train_garch_model, 
-    forecast_next_value_garch,
     load_garch_model,
     save_garch_model,
+    forecast_next_value_garch_steps,
 )
-from time_series.data import (
-    fetch_data, 
-    fetch_most_recent_data,
-)
+# from time_series.arima import (
+#     load_arima_model,
+#     train_arima_model,
+#     save_arima_model,
+#     forecast_next_value_arima,
+# )
+from time_series.data import fetch_data
 from nlp.news_sentiment import (
     get_company_name, 
     load_news_data, 
@@ -21,36 +24,17 @@ from nlp.news_sentiment import (
 
 app = Flask(__name__)
 socket_app = SocketIO(app)
-
-ticker = 'TSLA'
 garch_model = None
-data_queue = None
-pred_queue = deque(maxlen=10) # queue to store predictions for the last 10 minutes
+arima_model = None
 
-
-def scheduled_inference(ticker: str):
-    global garch_model, data_queue, pred_queue
-    # new_value = fetch_most_recent_data(ticker)
-    # print(new_value)
-    # data_queue.append(new_value)
-    # print(data_queue)
-    # garch_pred = forecast_next_value_garch(data_queue, garch_model)
-    # pred_queue.append(garch_pred)
-    # print(f"GARCH Prediction: {garch_pred}")
-    # print(pred_queue)
-    new_value = fetch_most_recent_data(ticker)
-    print(new_value)
-    data_queue.append(new_value)
-    print(data_queue)
-    garch_pred = forecast_next_value_garch(data_queue, garch_model)
-    pred_queue.append(garch_pred)
-    print(f"GARCH Prediction: {garch_pred}")
-    print(pred_queue)
+with open('TICKER.txt', 'r') as file:
+    ticker = file.read().strip()
 
 def initialize_ticker(ticker: str):
-    global garch_model, data_queue
+    global garch_model, data_queue, arima_model
     data = fetch_data(ticker)
     garch_model = load_garch_model(ticker)
+    # arima_model = load_arima_model(ticker)
     data_queue = deque(data[-10:], maxlen=10)
 
 def train_ticker(ticker: str, no_save: bool = False):
@@ -60,17 +44,24 @@ def train_ticker(ticker: str, no_save: bool = False):
     if not no_save:
         save_garch_model(ticker, garch_model)
 
-
 @app.route('/new_ticker', methods=['POST'])
 def new_ticker():
     data = request.json
     ticker = data['ticker']
     train_ticker(ticker)
+    with open('TICKER.txt', 'w') as file:
+        file.write(ticker)
+    return jsonify({"message": f"New ticker {ticker} trained."}), 200
 
-@app.route('/time_series_inference', methods=['GET'])
-def new_inference():
-    scheduled_inference(ticker)
-    return jsonify({"garch": pred_queue[-1]}), 200
+# steps = 1
+# @app.route('/time_series_inference', methods=['GET'])
+# def new_inference():
+#     global steps
+#     steps += 1
+#     garch_pred = forecast_next_value_garch_steps(steps, garch_model)
+#     print(steps)
+#     # arima_pred = forecast_next_value_arima(steps, arima_model)
+#     return jsonify({"garch": garch_pred}), 200
 
 @app.route('/news_sentiment', methods=['GET'])
 def news_sentiment():
@@ -89,5 +80,6 @@ def username(data):
     print(data)
 
 if __name__ == "__main__":
+    # train_arima_model(ticker)
     initialize_ticker(ticker)
     socket_app.run(app, debug=True, host='127.0.0.1', port=5000)
--- scheduler.py
+++ scheduler.py
@@ -1,25 +1,52 @@
 import time
+from collections import deque
 import threading
 import socketio
 
+from time_series.garch import (
+    forecast_next_value_garch,
+    load_garch_model,
+)
+from time_series.data import (
+    fetch_data, 
+    fetch_most_recent_data,
+)
+
 sio = socketio.Client(engineio_logger=True)
 
-def scheduled_function():
-    return "Hello, this is the scheduled function output!"
+with open('TICKER.txt', 'r') as file:
+    ticker = file.read().strip()
 
-def job():
+port = 5000
+host = f'http://127.0.0.1:{port}'
+garch_model = None
+data_queue = None
+pred_queue = deque(maxlen=10) # queue to store predictions for the last 10 minutes
+
+def initialize_ticker(ticker: str):
+    global garch_model, data_queue
+    data = fetch_data(ticker)
+    garch_model = load_garch_model(ticker)
+    data_queue = deque(data[-10:], maxlen=10)
+
+def scheduled_inference(ticker: str):
+    global garch_model, data_queue, pred_queue
+    new_value = fetch_most_recent_data(ticker)
+    data_queue.append(new_value)
+    garch_pred = forecast_next_value_garch(data_queue, garch_model)
+    pred_queue.append(garch_pred)
+
+def threaded_job():
     while True:
-        output = scheduled_function()
-        sio.emit('inference', {'data': output}, namespace='/schedule')
-        print(output)
-        time.sleep(60)  # wait for 1 minute
-
-def start_socket_connection():
-    sio.connect('http://127.0.0.1:5000', namespaces=['/schedule'])
-    sio.wait()
+        scheduled_inference(ticker)
+        output = pred_queue[-1]
+        sio.emit('inference', {'garch': output}, namespace='/schedule')
+        # print('\nSocket Output: ', output, end='\n')
+        time.sleep(60)
 
 if __name__ == "__main__":
-    sio.connect('http://127.0.0.1:5000', namespaces=['/schedule'])
-    job_thread = threading.Thread(target=job, daemon=True)
+    initialize_ticker(ticker)
+    sio.connect(host, namespaces=['/schedule'])
+    job_thread = threading.Thread(target=threaded_job, daemon=True)
     job_thread.start()
     sio.wait()
